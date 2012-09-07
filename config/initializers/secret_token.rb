c93102cb3a4093c4d27b613bf87265be78a335ca
mandar <mandar@mandar-System-Product-Name.(none)>
2012-09-07 13:32:44 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Herokutest::Application.config.secret_token = 'e091703f13fba0301a14605a65b97296e72cb132e141d8ebe416500fac291b4a8a2b9c5977a7af290cd5644bd36c1abbee04a59b869fd40c207f79db20781c4d'
